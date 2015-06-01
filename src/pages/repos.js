d6230f94dec5704a7c5fcc2c56e5d28c8062c76b
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-01 14:22:44 -0700

    Mon Jun  1 14:22:44 PDT 2015

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -1,6 +1,10 @@
+import app from 'ampersand-app'
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 export default React.createClass({
+  mixins: [ampersandMixin],
+
   render () {
     return (
       <div className='container'>
@@ -8,6 +12,9 @@ export default React.createClass({
           <h1>Repos</h1>
         </header>
         <div>
+          {this.props.repos.map((repo) => {
+            return <div>{repo.full_name}</div>
+          })}
         </div>
       </div>
     )
