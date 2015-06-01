1ef107844d78620453b58582931ad5cab0ba317b
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-01 09:50:00 -0700

    Mon Jun  1 09:50:00 PDT 2015

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import Router from './router'
 
-const Hello = React.createClass({
-  render () {
-    return <div>Hello, {this.props.name}</div>
+window.app = {
+  init () {
+    this.router = new Router()
+    this.router.history.start()
   }
-})
+}
 
-React.render(<Hello name='filive'/>, document.body)
+window.app.init()
