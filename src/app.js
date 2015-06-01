d069bf2f336cd513d70cd14a560eb22ae02a5426
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-01 09:41:46 -0700

    Mon Jun  1 09:41:46 PDT 2015

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-document.write('Hello FI Live!')
+import React from 'react'
+
+const Hello = React.createClass({
+  render () {
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name='filive'/>, document.body)
