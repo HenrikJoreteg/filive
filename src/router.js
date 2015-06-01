d192a560bb21242ce47f4cb6efc74a976ad260f1
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-01 11:44:53 -0700

    Mon Jun  1 11:44:53 PDT 2015

--- src/router.js
+++ src/router.js
@@ -2,11 +2,15 @@ import Router from 'ampersand-router'
 import React from 'react'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
+import qs from 'qs'
+import xhr from 'xhr'
 
 export default Router.extend({
   routes: {
     '': 'public',
-    'repos': 'repos'
+    'repos': 'repos',
+    'login': 'login',
+    'auth/callback?code=:code': 'authCallback'
   },
 
   public () {
@@ -15,5 +19,24 @@ export default Router.extend({
 
   repos () {
     React.render(<ReposPage/>, document.body)
+  },
+
+  login () {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      scope: 'user,repo',
+      redirect_uri: window.location.origin + '/auth/callback',
+      client_id: 'f8dd69187841cdd22a26'
+    })
+  },
+
+  authCallback (code) {
+    console.log(code)
+
+    xhr({
+      url: 'http://labelr-dev.herokuapp.com/authenticate/' + code,
+      json: true
+    }, (err, req, body) => {
+      console.log(body)
+    })
   }
 })
