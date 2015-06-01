851fff6bcbaa06554bd9d03474c83c8023aa4120
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-01 10:01:00 -0700

    Mon Jun  1 10:01:00 PDT 2015

---
+++ src/pages/repos.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <header role='banner'>
+          <h1>Repos</h1>
+        </header>
+        <div>
+        </div>
+      </div>
+    )
+  }
+})
